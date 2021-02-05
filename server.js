effeca0ef833f2e88797bd19cf1c184542ef8656
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-02-05 20:49:18 +0700

    Removed @octokit/request and add compression

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const compression = require("compression");
 const express = require("express");
-const app = express();
 
+const app = express();
 const PORT = 7000 || process.env.PORT;
 
-app.get("/", (req, res) => res.send("Hello"));
+app.use(compression());
+
+app.get("/", (req, res) => res.send('Go to <a href="/api">/api</a>'));
 
 app.listen(PORT, () =>
   console.log(`Listening on port ${PORT} | http://localhost:${PORT}`)
